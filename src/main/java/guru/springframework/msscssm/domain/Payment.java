3adbc3c2945eb33224b44404a2d656a137b75b29
Gianni Fabriziani <gianni.fabriziani@gmail.com>
2019-09-27 10:55:06 +0200

    add states, events and entity with repository

---
+++ src/main/java/guru/springframework/msscssm/domain/Payment.java
@@ -0,0 +1,25 @@
+package guru.springframework.msscssm.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Payment {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentState paymentState;
+
+    private BigDecimal amount;
+}
